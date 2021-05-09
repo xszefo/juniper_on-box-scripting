34fe5fea1f3da55d3dc0b2a5def80c3f8fcfd963
Piotr Sumelka <piotr.sumelka@gmail.com>
2021-05-09 09:48:50 -0700

    puste interfejsy

--- python/op/show_int_descr_and_ip.py
+++ python/op/show_int_descr_and_ip.py
@@ -15,9 +15,11 @@ if __name__ == '__main__':
 	for interface in interfaces_description.iter('physical-interface'):
 		name = interface.findtext('name').strip()
 		description = interface.findtext('description').strip()
-
 		ip_xpath ='//physical-interface[normalize-space(name)=$name]/logical-interface/address-family/interface-address/ifa-local'
 		ip_result = interfaces_terse.xpath(ip_xpath, name=name)
-		for ip in ip_result:
-			ip = ip.text.strip()
-			print '{} {} {}'.format(name, description, ip)
+		if ip_result:
+			for ip in ip_result:
+				ip = ip.text.strip()
+				print '{} {} {}'.format(name, description, ip)
+		else:	
+			print '{} {}'.format(name, description)
