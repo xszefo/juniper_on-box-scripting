6b483937f17fe7d9223f26d233d0e3998434a507
Piotr Sumelka <piotr.sumelka@gmail.com>
2021-05-09 09:38:08 -0700

    poprawa formatowania

--- python/op/show_int_descr_and_ip.py
+++ python/op/show_int_descr_and_ip.py
@@ -6,12 +6,12 @@ if __name__ == '__main__':
 		interfaces_terse = dev.rpc.get_interface_information(terse=True)
 		interfaces_description = dev.rpc.get_interface_information(descriptions=True)
 		
-		for interface in interfaces_description.iter('physical-interface'):
-			name = interface.findtext('name').strip()
-			description = interface.findtext('description').strip()
+	for interface in interfaces_description.iter('physical-interface'):
+		name = interface.findtext('name').strip()
+		description = interface.findtext('description').strip()
 
-			ip_xpath ='//physical-interface[normalize-space(name)=$name]/logical-interface/address-family/interface-address/ifa-local'
-			ip_result = interfaces_terse.xpath(ip_xpath, name=name)
-			for ip in ip_result:
-				ip = ip.text.strip()
-				print '{} {} {}'.format(name, description, ip)
+		ip_xpath ='//physical-interface[normalize-space(name)=$name]/logical-interface/address-family/interface-address/ifa-local'
+		ip_result = interfaces_terse.xpath(ip_xpath, name=name)
+		for ip in ip_result:
+			ip = ip.text.strip()
+			print '{} {} {}'.format(name, description, ip)
