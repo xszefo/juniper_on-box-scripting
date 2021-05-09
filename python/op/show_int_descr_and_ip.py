8c5dacebb5538ab8b6d4b3671743af35272e0ff0
Piotr Sumelka <piotr.sumelka@gmail.com>
2021-05-09 09:43:30 -0700

    obsluga bledow

--- python/op/show_int_descr_and_ip.py
+++ python/op/show_int_descr_and_ip.py
@@ -1,11 +1,17 @@
 from jnpr.junos import Device
 from lxml import etree
+import sys
 
 if __name__ == '__main__':
 	with Device() as dev:
 		interfaces_terse = dev.rpc.get_interface_information(terse=True)
 		interfaces_description = dev.rpc.get_interface_information(descriptions=True)
-		
+
+	if type(interfaces_terse) != etree._Element or type(interfaces_description) != etree._Element:
+		print('Blad pobierania danych')
+		print('Terse - {}\nDescription - {}'.format(type(interfaces_terse), type(interfaces_description)))
+		sys.exit()
+
 	for interface in interfaces_description.iter('physical-interface'):
 		name = interface.findtext('name').strip()
 		description = interface.findtext('description').strip()
