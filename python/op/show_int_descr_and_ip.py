2c807b65162cc0580eff43a64715ff552f43d103
Piotr Sumelka <piotr.sumelka@gmail.com>
2021-11-16 12:54:29 +0100

    formatowanie

--- python/op/show_int_descr_and_ip.py
+++ python/op/show_int_descr_and_ip.py
@@ -3,23 +3,25 @@ from lxml import etree
 import sys
 
 if __name__ == '__main__':
-	with Device() as dev:
-		interfaces_terse = dev.rpc.get_interface_information(terse=True)
-		interfaces_description = dev.rpc.get_interface_information(descriptions=True)
+    with Device() as dev:
+	interfaces_terse = dev.rpc.get_interface_information(terse=True)
+	interfaces_description = dev.rpc.get_interface_information(descriptions=True)
 
-	if type(interfaces_terse) != etree._Element or type(interfaces_description) != etree._Element:
-		print('Blad pobierania danych')
-		print('Terse - {}\nDescription - {}'.format(type(interfaces_terse), type(interfaces_description)))
-		sys.exit()
+    if type(interfaces_terse) != etree._Element or type(interfaces_description) != etree._Element:
+	print('Blad pobierania danych')
+	print('Terse - {}\nDescription - {}'.format(type(interfaces_terse), type(interfaces_description)))
+	sys.exit()
 
-	for interface in interfaces_description.iter('physical-interface'):
-		name = interface.findtext('name').strip()
-		description = interface.findtext('description').strip()
-		ip_xpath ='//physical-interface[normalize-space(name)=$name]/logical-interface/address-family/interface-address/ifa-local'
-		ip_result = interfaces_terse.xpath(ip_xpath, name=name)
-		if ip_result:
-			for ip in ip_result:
-				ip = ip.text.strip()
-				print '{} {} {}'.format(name, description, ip)
-		else:	
-			print '{} {}'.format(name, description)
+    print '{:15} {:20} {:15}'.format('Name', 'Description', 'IP')
+    for interface in interfaces_description.iter('physical-interface'):
+	name = interface.findtext('name').strip()
+	description = interface.findtext('description').strip()
+	ip_xpath ='//physical-interface[normalize-space(name)=$name]/logical-interface/address-family/interface-address/ifa-local'
+	ip_result = interfaces_terse.xpath(ip_xpath, name=name)
+	if ip_result:
+	    for ip in ip_result:
+		ip = ip.text.strip()
+                print '{:15} {:20} {:15}'.format(name, description, ip)
+	else:	
+            print '{:15} {:20}'.format(name, description)
+
