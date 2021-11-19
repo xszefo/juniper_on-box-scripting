8966f0c2e83db18403ad4af541a5e3b47b8d9fc2
Piotr Sumelka <piotr.sumelka@gmail.com>
2021-11-19 10:57:13 +0100

    zmiany w formatowaniu

--- python/op/show_int_descr_and_ip.py
+++ python/op/show_int_descr_and_ip.py
@@ -6,37 +6,37 @@ def get_details(interfaces_terse, interfaces_description):
     result = []
 
     for interface in interfaces_description.iter('physical-interface'):
-	name = interface.findtext('name').strip()
-	description = interface.findtext('description').strip()
-	ip_xpath ='//physical-interface[normalize-space(name)=$name]/logical-interface/address-family/interface-address/ifa-local'
-	ip_result = interfaces_terse.xpath(ip_xpath, name=name)
-	ip = []
+        name = interface.findtext('name').strip()
+        description = interface.findtext('description').strip()
+        ip_xpath ='//physical-interface[normalize-space(name)=$name]/logical-interface/address-family/interface-address/ifa-local'
+        ip_result = interfaces_terse.xpath(ip_xpath, name=name)
+        ip = []
         if len(ip_result) > 0:
-	    for tmp_ip in ip_result:
-		ip.append(tmp_ip.text.strip())
-        result.append((name, description, ip))
+            for tmp_ip in ip_result:
+                ip.append(tmp_ip.text.strip())
+        result.append((name, name, ip))
     return result
     
 def main():
     with Device() as dev:
-	interfaces_terse = dev.rpc.get_interface_information(terse=True)
-	interfaces_description = dev.rpc.get_interface_information(descriptions=True)
+        interfaces_terse = dev.rpc.get_interface_information(terse=True)
+        interfaces_description = dev.rpc.get_interface_information(descriptions=True)
 
     if type(interfaces_terse) != etree._Element or type(interfaces_description) != etree._Element:
-	print('Blad pobierania danych')
-	print('Terse - {}\nDescription - {}'.format(type(interfaces_terse), type(interfaces_description)))
-	sys.exit()
+        print('Blad pobierania danych')
+        print('Terse - {}\nDescription - {}'.format(type(interfaces_terse), type(interfaces_description)))
+        sys.exit()
 
     result = get_details(interfaces_terse, interfaces_description)
 
-    print '{:15} {:20} {:15}'.format('Name', 'Description', 'IP')
+    print('{:15} {:20} {:15}'.format('Name', 'Description', 'IP'))
     for name, description, ips in result:
         if len(ips) > 0:
             print('{:15} {:20} {:15}'.format(name, description, ips[0]))
+            for ip in ips[1:]:
+                print('{:15} {:20} {:15}'.format('','', ip))
         else:
             print('{:15} {:20}'.format(name, description))
-        for ip in ips[1:]:
-            print('{:15} {:20} {:15}'.format('','', ips[0]))
 
 if __name__ == '__main__':
     main()
